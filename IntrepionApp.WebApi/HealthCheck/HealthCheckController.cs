188f149db8daca45dffdc77b422f5af1b8c65f76
Oliver Forral <intrepion@gmail.com>
2023-01-17 07:21:51 -0700

    green - testing the health check endpoint

--- IntrepionApp.WebApi/HealthCheck/HealthCheckController.cs
+++ IntrepionApp.WebApi/HealthCheck/HealthCheckController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace IntrepionApp.WebApi.HealthCheck;
 
 [ApiController]
+[Route("[controller]")]
 public class HealthCheckController : ControllerBase
 {
     public IActionResult Get()
