8054827bc13f6a6f598af6847118bd8677957006
Oliver Forral <intrepion@gmail.com>
2023-01-17 07:22:03 -0700

    red - testing the health check controller for 200 status

---
+++ IntrepionApp.Tests/WebApi/JsonRpc/TestJsonRpcController.cs
@@ -0,0 +1,11 @@
+namespace IntrepionApp.Tests.WebApi.JsonRpc;
+
+public class TestJsonRpcController
+{
+    [Fact]
+    public void Get_Returns200()
+    {
+        // Arrange
+        var controller = new JsonRpcController();
+    }
+}
