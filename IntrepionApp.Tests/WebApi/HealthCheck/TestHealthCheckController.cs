3bee458b30de5162cd54100e96c2fcc83f0fe4cf
Oliver Forral <intrepion@gmail.com>
2023-01-17 07:19:50 -0700

    red - testing the health check controller for 200 status

---
+++ IntrepionApp.Tests/WebApi/HealthCheck/TestHealthCheckController.cs
@@ -0,0 +1,11 @@
+namespace IntrepionApp.Tests.WebApi.HealthCheck;
+
+public class TestHealthCheckController
+{
+    [Fact]
+    public void Get_Returns200()
+    {
+        // Arrange
+        var controller = new HealthCheckController();
+    }
+}
